53b9d7e31f20fa6e9f7a60903e07a94b231c5dd9
kctier <joe.tester.j@gmail.com>
2016-02-23 04:05:27 +0300

    Copypaste tcp server/client from official docs

---
+++ client/main.py
@@ -0,0 +1,25 @@
+import asyncio
+
+
+async def tcp_echo_client(message, *, loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888, loop=loop)
+
+    print('Send: %r' % message)
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print('Received: %r' % data.decode())
+
+    print('Close the socket')
+    writer.close()
+
+
+def run_client():
+    message = 'Hello World!'
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tcp_echo_client(message, loop=loop))
+    loop.close()
+
+
+if __name__ == '__main__':
+    run_client()
